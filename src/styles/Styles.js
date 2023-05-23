cd70382274c7dd42214bd539031f9aa845bb54ff
Vitor Mateus <vmateusaguiar@gmail.com>
2023-05-22 21:33:42 -0300

    feat: finishing simple calculator project

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -5,8 +5,23 @@ export const MainContainer = styled.main`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    /* width: 100%; */
-    /* height: 500px; */
+    width: 100%;
+    /* background-color: #222; */
+`;
+
+export const NavBar = styled.nav`
+    display: flex;
+    align-items: center;
+    height: 72px;
+    width: 100%;
+    background-color: yellow;
+    margin-bottom: 80px;
+    padding: 0 48px;
+
+    p {
+        font-size: 40px;
+        font-weight: bold;
+    }
 `;
 
 export const Row = styled.div`
@@ -35,9 +50,24 @@ export const Screen = styled.div`
     align-items: center;
     font-size: 40px;
     height: 96px;
-    width: 325px;
+    width: 345px;
     background-color: #d1edf6;
     border-radius: 8px;
     padding: 0 8px;
     overflow: hidden;
+`;
+
+export const Footer = styled.footer`
+    display: flex;
+    align-items: end;
+    justify-content: center;
+    height: 148px;
+    width: 100%;
+    background-color: yellow;
+    margin-top: 80px;
+    padding: 24px 0px;
+
+    span {
+        font-weight: bold;
+    }
 `;
